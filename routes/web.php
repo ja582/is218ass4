fd4293844af8c8d34232bf13fd6eb05c953717cf
joe <ja582@njit.edu>
2019-07-21 19:23:13 -0400

    Updating files

--- routes/web.php
+++ routes/web.php
@@ -12,11 +12,14 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.index');
+});
+Route::get('/about', function () {
+    return view('pages.about');
 });
 Route::get('/contact', function () {
-    return view('welcome');
+    return view('pages.contact');
 });
-Route::post('/thankyou', function () {
+Route::post('/contact', function () {
     return view('welcome');
 });
